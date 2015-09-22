27faefec7311296ce0088e1abcf055b786c11ca2
Dominic Barnes <dominic@dbarnes.info>
2015-09-22 17:55:41 +0000

    adding eslint, making fixes

--- preview/index.js
+++ preview/index.js
@@ -2,21 +2,21 @@
 /** @jsx element */
 
 import element from 'dekujs/virtual-element';
-import {tree,render} from 'dekujs/deku';
-import {Icon,names} from '../';
+import { tree, render } from 'dekujs/deku';
+import { Icon, names } from '../';
 
-function renderIcons (size, color) {
+function renderIcons(size, color) {
   return (
     <div style="margin: 25px auto; width: 700px; overflow: hidden">
-      {names.map(function(name){
+      {names.map(function(name) {
         return (
           <div style="float:left; margin: 10px; background: whiteSmoke;">
             <Icon type={name} size={size} color={color} />
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 let Preview = {
@@ -27,11 +27,8 @@ let Preview = {
         {renderIcons('32px', '#CCBEAD')}
         {renderIcons('64px', '#3CC76A')}
       </div>
-    )
+    );
   }
-}
+};
 
-render(
-  tree(<Preview />),
-  document.body
-)
+render(tree(<Preview />), document.body);
